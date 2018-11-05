2684684139e79289f3b7faafdc327706c2e6f767
PrinceOfShapeir <somethingfarley@gmail.com>
2018-11-04 23:56:22 -0500

    Update tictac.js
    Updated with dynamic wincheck function using mathematical sums.
    Reduced number of useless moves.

--- tictac.js
+++ tictac.js
@@ -27,6 +27,120 @@ function Perceptron(input, hidden, output){
 
 }
 
+function sum (array){
+	let s = 0;
+	for(i in array){
+		
+		s += array[i];
+	}
+	return s;
+}
+
+function win(input){
+	
+	let side = Math.sqrt(input.length);
+	let rows = [];
+	let columns = [];
+	let newColumns = []
+	let diags = new Array(2);
+	diags = diags.fill([]);
+	/*
+	let columns = new Array(side);
+	columns = columns.fill([]);*/
+
+for(let i = 0; i<input.length;){
+rows.push(input.slice(i,side+i));
+i += side;
+}
+
+//console.log(rows);
+//return true;
+for(i in rows){
+	
+	if(sum(rows[i])===side){
+		console.log("row win");
+		return true;
+	}
+	
+	
+}
+
+
+
+for(i in rows){
+	
+	
+	for(let z = 0; z<side;z++){
+	
+		
+		columns.push(rows[z][i]);
+		
+	}
+	
+}
+	
+	//console.log(columns); return true;
+	for(let i = 0; i<columns.length;){
+newColumns.push(columns.slice(i,side+i));
+i += side;
+}
+
+//console.log(newColumns);
+//}
+
+
+//console.log(rows);
+//console.log(columns);
+//return true;
+for (i in newColumns){
+	//console.log(sum(columns[i])===side);
+	if(sum(newColumns[i])===side){
+		console.log("column win");
+		return true;
+	}
+	
+}
+	/*
+	
+	
+	
+	let inputString = input.slice().toString();
+	let winTest = /(1,(0.5|0),(0.5|0),1,(0.5|0),(0.5|0),1)|/
+	return /(1,){3}|1\d\d1\d\d1/.test(inputString);	
+	
+	*/
+	
+	for(i in input){
+		
+	diags[0].push(input[i]);
+		
+	i+=side+1;
+	}
+	
+	for(let i = side -1; i<input.length;){
+		
+		diags[1].push(input[i]);
+		
+		i += side - 1;
+		
+	}
+	
+	for (i in diags){
+	
+	if(sum(diags[i])===side){
+		console.log("diagonal win");
+		return true;
+	}
+}
+	//console.log(diags);
+	//return true;
+	return false;
+	
+	
+	//checks rows
+}
+
+
 
 Perceptron.prototype = new Network();
 Perceptron.prototype.constructor = Perceptron;
@@ -76,21 +190,50 @@ output: [1]
 
 { input: [.5,.5,.5,.5,1,.5,.5,.5,.5],
 	output: [1]	
-}, 
-//moves that aren't winning moves are considered bad ones
-//depending on how the training algo works, it either evaluates this every pass, or evaluates once and trains the same hardcoded value
-//obviously we prefer the former
-{ input: [Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random()],
-	output: [0]
 }
 
 ]
 
+var mustSet = [{ input: [.5,.5,.5,.5,1,.5,.5,.5,.5],
+	output: [1]	
+}];
 
+var mustSetInit = [.5,.5,.5,.5,1,.5,.5,.5,.5];
+
+for(let i = 0; i<8; i++){
+	
+	
+	mustSetInit.push(mustSetInit.shift());
+
+	mustSet.push({
+		
+		input: mustSetInit,
+		output: [0]
+		
+	}
+	);
+	
+}
+
+var nullSet = [];//random moves are considered poor
+for(let i = 0; i<500;i++){
+
+nullSet.push({ input: [Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random()],
+	output: [0]
+});	
+	
+}
 
 ticTacTrainer.train(trainSet);
+ticTacTrainer.train(mustSet);
+ticTacTrainer.train(nullSet);
 //moves contains the potentially evaluated move, .5 is the neutral state.
-var moves = [.5,.5,.5,.5,.5,.5,.5,.5,.5];
+
+//var gridMap = [.5,.5,.5,.5,.5,.5,.5,.5,.5];
+
+var game = function(moves, count){
+
+//var moves = [.5,.5,.5,.5,.5,.5,.5,.5,.5];
 var movesRated = [];
 
 for (var i = 0; i<9; i++) {
@@ -100,20 +243,60 @@ for (var i = 0; i<9; i++) {
 	if(moveCache[i]===.5){
 	moveCache[i] = 1;
 	
-	movesRated.push(ticTacPlayer.activate(moveCache));
+	let rating = ticTacPlayer.activate(moveCache);
+	movesRated.push(rating[0]);
 	
-	console.log(movesRated[i] + " " + i + 1);
+	//console.log(movesRated[i] + " " + i + 1);
+	//console.log(ticTacPlayer.activate(moveCache));
 	
 }
 else {
 movesRated.push(0); 
-console.log("move " + i + " already taken");
+//console.log("move " + i + " already taken");
 }
 	
 	}
+	
+let move = movesRated.indexOf(Math.max.apply(Math,movesRated))+1;
 
-console.log(Math.max.apply(Math,movesRated));
+moves[move-1] = 1;
+
+console.log(move);
+console.log(moves.slice(0,3));
+console.log(moves.slice(3,6));
+console.log(moves.slice(6));
+//console.log(moves);
+//console.log(movesRated);
 
 //console.log(ticTacPlayer.activate([.5,.5,.5,.5,1,.5,.5,.5,.5]));
 
-console.log("it's alive!");
+//console.log("it's alive!");
+if(win(moves)){
+	console.log("win");
+	console.log(count);
+	return moves;
+}
+else if(count>=9){
+	console.log("ran out of moves");
+	return moves;
+	}
+else if(moves.indexOf(.5)===-1){
+	console.log("tie");
+	//console.log(moves);	
+	return moves;
+	
+}
+
+else {
+count++;
+return game(moves, count);
+
+}
+
+}
+
+game(.5,.5,.5,.5,.5,.5,.5,.5,.5,], 1);
+
+//This is single player tic tac toe
+//theoretically the network should only take 3 moves to win by itself
+//any more than that and it is not efficient. 
