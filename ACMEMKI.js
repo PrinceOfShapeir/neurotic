023e76c6c62da4f3d4e63609376b2517441d2bb1
PrinceOfShapeir <somethingfarley@gmail.com>
2018-11-07 11:08:07 -0500

    Basic type checking + function to invert normalized arrays.

--- ACMEMKI.js
+++ ACMEMKI.js
@@ -2,6 +2,11 @@
 
 
 exports.sum = function (array){
+	if(!Array.isArray(array)){
+	
+	return "Error not an array";
+	}
+	
 	let s = 0;
 	for(i in array){
 		
@@ -10,3 +15,20 @@ exports.sum = function (array){
 	return s;
 }
 
+// returns the inverse of any normalized array
+exports.flip = function (array) {
+	
+	if(!Array.isArray(array)){
+	
+	return "Error not an array";
+	}
+	
+
+		for(i in array){
+		
+			array[i] = Math.abs(array[i]-1);
+		}
+	
+	return array;
+	
+}
